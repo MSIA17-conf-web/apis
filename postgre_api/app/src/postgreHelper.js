7df4732ccaadb1938f5284927e486589afde8606
Willem Houm <willem.houm@gmail.com>
2019-11-06 14:20:55 +0000

    Renewed postgre API with pgpromises

--- postgre_api/app/src/postgreHelper.js
+++ postgre_api/app/src/postgreHelper.js
@@ -1,120 +1,57 @@
-const { Pool } = require('pg');
+require("dotenv").config();
 
-let pool = null;
+const pgp = require('pg-promise')({
+    error(error, e) {
+        if (e.cn) {
+            console.log('CN:', e.cn);
+            console.log('EVENT:', error.message || error);
+        }
+    }
+}),
+    path = require('path');
 
+const cn = {
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port
+};
 
-let postgreHelper = {
+var db = pgp(cn);
+
+module.exports = {
     initConnection: () => {
         return new Promise((resolve, reject) => {
-            console.log("Connecting to postgre database...");
-
-            pool = new Pool({
-                user: 'msia17',
-                host: 'postgres',
-                database: 'msia17conferences',
-                password: 'msia17',
-                port: 5432,
-            });
-            pool
-                .query('SELECT NOW()')
-                .then(res => {
-                    console.log(res.rows[0].now)
-                    resolve('Connection to postgre OK');
+            db.proc('version')
+                .then(data => {
+                    resolve("Connection to db ok : " + data.version)
                 })
                 .catch(err => {
+
                     reject(err)
                 })
+        });
+    },
+    execFromFile: (body) => {
+        return new Promise((resolve, reject) => {
+            db.any(loadQueryFromFile(body.fileName), body.options)
+                .then(res => {
+                    console.log("Got result for",body.fileName);
+                    console.log(res);
+                    
+                    resolve(res);
+                })
+                .catch(err => {
+                    console.log("Got error for",body.fileName);
+
+                    reject(err);
+                })
         })
-    },
-    getAll: body => {
-        return new Promise((resolve, reject) => {
-            const sqlQuery = 'SELECT * FROM ' + body.table + ' ORDER BY "' + body.orderBy + '" ASC';
-
-            pool.query(sqlQuery, (err, results) => {
-                if (err) {
-                    reject({ err: err });
-                }
-                console.log("Récupération de la liste des personnes inscrites");
-
-                resolve({ result: results.rows });
-            });
-
-        });
-    },
-    createGuest: body => {
-        return new Promise((resolve, reject) => {
-            let newId;
-
-            pool.query('SELECT MAX("idGuest") FROM ' + body.table, (err, results) => {
-                if (err) {
-                    reject({ err: err });
-                }
-                console.log("Incrémentation du dernier identifiant");
-                console.log("result", results.rows[0].max);
-
-                newId = results.rows[0].max + 1;
-                if (newId) {
-                    const sqlQuery = 'INSERT INTO ' + body.table + ' ("idGuest", "lastName", "firstName", "enterpriseName", "email") '
-                        + 'VALUES (' + newId + ', \'' + body.lastName + '\', \'' + body.firstName + '\', \'' + body.enterpriseName + '\''
-                        + ', \'' + body.email + '\');';
-
-                    pool.query(sqlQuery, (err, results) => {
-                        if (err) {
-                            reject({ err: err });
-                        }
-                        console.log("La réservation a bien été enregistré pour l'utilisateur " + body.lastName);
-
-                        resolve({ result: true });
-                    });
-                } else {
-                    reject({ err: "Impossible de récupérer le nouvel identifiant" });
-                }
-            });
-        });
-    },
-    updateGuest: body => {
-        return new Promise((resolve, reject) => {
-
-            let id = body.id, lastName = body.lastName, firstName = body.firstName, enterpriseName = body.enterpriseName, email = body.email;
-
-
-            // faire une requete entiere et non avec des if
-            if (lastName && firstName && enterpriseName && email) {
-                let sqlQuery = 'UPDATE ' + body.table + ' SET "lastName" = \'' + lastName + '\', "firstName" = \'' + firstName + '\', '
-                    + '"enterpriseName" = \'' + enterpriseName + '\', "email" = \'' + email + '\' WHERE "idGuest" = ' + id;
-
-                console.log("sqlQuery", sqlQuery);
-
-                pool.query(sqlQuery, (err, results) => {
-                    console.log("test");
-
-                    if (err) {
-                        reject({ err: err });
-                    }
-                    console.log("L'utilisateur avec l'identifiant " + id + " a bien été modifié");
-
-                    resolve({ result: true });
-                });
-            } else {
-                reject({ err: "Tous les champs sont obligatoires" });
-            }
-        });
-    },
-    removeGuest: body => {
-        return new Promise((resolve, reject) => {
-            const sqlQuery = 'DELETE FROM ' + body.table + ' WHERE "idGuest" = ' + body.id;
-
-            pool.query(sqlQuery, (err, results) => {
-                if (err) {
-                    reject({ err: err });
-                }
-                console.log("L'utilisateur avec l'edentifiant " + body.id + " a bien été supprimé");
-
-                resolve({ result: true });
-            });
-        });
     }
-
 }
 
-module.exports = postgreHelper;
+function loadQueryFromFile(file) {
+    const fullPath = path.join(__dirname, "queries", file + ".sql");
+    return new pgp.QueryFile(fullPath , { minify: true });
+}
--- postgre_api/app/src/router.js
+++ postgre_api/app/src/router.js
@@ -1,129 +1,61 @@
 require("dotenv").config();
 
 const cors = require("cors"),
-  express = require("express"),
-  app = express(),
-  http = require("http"),
-  // https = require("https"),
-  bodyParser = require("body-parser"),
-  postgreHelper = require("./postgreHelper");
+    express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    postgreHelper = require("./postgreHelper");
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// GET — / | displayHome()
-// GET — /users | getUsers()
-// GET — /users/:id | getUserById()
-// POST — users | createUser()
-// PUT — /users/:id | updateUser()
-// DELETE — /users/:id | deleteUser()
-
-app.get("/*", (req, res, next) => {
-  console.log("Request", req.path);
-  next();
+app.all("/*", (req, res, next) => {
+    console.log("Request on ", req.path);
+    next();
 });
 
-app.get("/test", (req, res) => {
-  let result = postgreHelper.getTest()
-  console.log("/test", result);
-
-  res.send("ok").end();
-});
-
-app.get("/getAllGuest", (req, res) => {
-  let body = req.body;
-
-  let error = checkBody(body.table, body.orderBy)
-
-  if (error.length != 0) {
-    console.log("Error calling getAllGuest", error);
-    res.send(error).end();
-  } else {
-    postgreHelper.getAll(body)
-      .then(result => res.send(result).end())
-      .catch(err => res.send(err).end());
-  }
-});
-
-app.post("/createGuest", (req, res) => {
-  let body = req.body;
-
-  let error = checkBody(body.table, false, body.lastName, body.firstName, body.enterpriseName, body.email);
-
-  if (error.length != 0) {
-    console.log("Error calling createGuest", error);
-    res.send(error).end();
-  } else {
-    postgreHelper.createGuest(body)
-      .then(result => res.send(result).end())
-      .catch(err => res.send(err).end());
-  }
-});
-
-app.put("/updateGuest", (req, res) => {
-  let body = req.body;
-
-  let error = checkBody(body.table, false, body.lastName, body.firstName, body.enterpriseName, body.email, body.id);
-
-  if (error.length != 0) {
-    console.log("Error calling createGuest", error);
-    res.send(error).end();
-  } else {
-    postgreHelper.updateGuest(body)
-      .then(result => res.send(result).end())
-      .catch(err => res.send(err).end());
-  }
-});
-
-app.delete("/removeGuest", (req, res) => {
-  let body = req.body;
-
-  postgreHelper.removeGuest(body)
-    .then(result => res.send(result).end())
-    .catch(err => res.send(err).end());
-});
+app.post("/from-file", (req, res) => {
+    checkBody("from-file", req.body, err => {
+        if (err.length != 0) {
+            console.log(err);
+            res.send({ err: err }).end()
+        } else {
+            postgreHelper.execFromFile(req.body)
+                .then(data => {                  
+                    
+                    res.send({ res: data.map((tmp => { return tmp.json})) }).end();
+                })
+                .catch(err => {
+                    console.log('prout', err);
+                    
+                    res.send({ err: err }).end()
+                })
+        }
+    })
+})
 
 postgreHelper.initConnection().then(ok => {
-  console.log(ok);
-  app.listen(process.env.SERVER_PORT, () => {
-    console.log("Postgre API launched on port " + process.env.SERVER_PORT);
-  });
+    console.log(ok);
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log("Postgre API launched on port " + process.env.SERVER_PORT);
+    });
 }).catch(err => {
-  console.log('Error connecting to DB', err);
+    console.log('Error connecting to DB', err);
 });
 
-function checkBody(table, orderBy, lastName, firstName, enterpriseName, email, id) {
-  let error = [];
+function checkBody(method, body, cb) {
+    let err = [];
 
-  if (!table || !orderBy || !lastName || !firstName || !enterpriseName || !email || id) {
-    if (!table) {
-      error.push({ errTable: true });
+    switch (method) {
+        case "from-file":
+
+            break;
+
+        default:
+            err.push({ methodNotExist: "The method" + method + "isn't supported" })
+            break;
     }
 
-    if (!orderBy) {
-      error.push({ errOrderBy: true });
-    }
 
-    if (!lastName) {
-      error.push({ errLastName: true });
-    }
-
-    if (!firstName) {
-      error.push({ errFirstName: true });
-    }
-
-    if (!enterpriseName) {
-      error.push({ errEnterpriseName: true });
-    }
-
-    if (!email) {
-      error.push({ errEmail: true });
-    }
-
-    if (!id) {
-      error.push({ errId: true });
-    }
-  }
-
-  return error;
+    cb(err)
 }
